8601057e89abb47284364317199d502f54d5ab63
Sai Deepak <psaideepak1@gmail.com>
2024-06-11 23:19:11 +0530

    Navbar Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="z-10 flex min-h-[16rem] items-center justify-center rounded-lg border bg-white dark:bg-black">
+      <div className="z-10 flex min-h-[16rem] items-center justify-center rounded-lg bg-white dark:bg-black">
         <TextReveal text="I am Currently learning Machine Learning, AWS, Next.js, Sring Boot." />
       </div>
     </div>
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,17 +3,19 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import Link from "next/link"
 import Image from "next/image"
 import logodark from "../../../public/logo_dark.svg"
+import logolight from "../../../public/logo_light.svg"
 import {Menu, Package2} from "lucide-react"
 import { ThemeToggle } from './theme-toggle';
 
 export function Navbar() {
 return(
-    <header className="sticky top-0 flex h-16 justify-between items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="sticky top-0 flex h-16 justify-between items-center gap-4 bg-background dark:bg-black px-4 md:px-6">
         <Link
             href="#"
             className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
-            <Image className="h-6 w-6" src={logodark} alt="Logo D"/>
+            <Image className="h-6 w-6 block dark:hidden" src={logodark} alt="Logo D"/>
+            <Image className="h-6 w-6 hidden dark:block" src={logolight} alt="Logo D"/>
             <span className="sr-only">Sai Deepak Portfolio</span>
         </Link>
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
@@ -21,32 +23,33 @@ return(
             href="#"
             className="text-muted-foreground transition-colors hover:text-foreground"
           >
-            Dashboard
+            About Me
           </Link>
           <Link
             href="#"
             className="text-muted-foreground transition-colors hover:text-foreground"
           >
-            Orders
+            Education
           </Link>
           <Link
             href="#"
             className="text-muted-foreground transition-colors hover:text-foreground"
           >
-            Products
+            Projects
           </Link>
           <Link
             href="#"
             className="text-muted-foreground transition-colors hover:text-foreground"
           >
-            Customers
+            Resume
           </Link>
           <Link
             href="#"
-            className="text-foreground transition-colors hover:text-foreground"
+            className="text-muted-foreground transition-colors hover:text-foreground"
           >
-            Settings
+            Contact Me
           </Link>
+          <ThemeToggle/>
         </nav>
         <Sheet>
           <SheetTrigger asChild>
@@ -54,7 +57,7 @@ return(
               variant="outline"
               size="icon"
               className="shrink-0 md:hidden"
-            >
+              >
               <Menu className="h-5 w-5" />
               <span className="sr-only">Toggle navigation menu</span>
             </Button>
@@ -65,39 +68,40 @@ return(
                 href="#"
                 className="flex items-center gap-2 text-lg font-semibold"
               >
-                <Package2 className="h-6 w-6" />
-                <span className="sr-only">Acme Inc</span>
+                <Image className="h-6 w-6 block dark:hidden" src={logodark} alt="Logo D"/>
+                <Image className="h-6 w-6 hidden dark:block" src={logolight} alt="Logo D"/>
+                <span className="sr-only">Sai Deepak Portfolio</span>
               </Link>
               <Link
                 href="#"
                 className="text-muted-foreground hover:text-foreground"
               >
-                Dashboard
+                About Me
               </Link>
               <Link
                 href="#"
                 className="text-muted-foreground hover:text-foreground"
               >
-                Orders
+                Education
               </Link>
               <Link
                 href="#"
                 className="text-muted-foreground hover:text-foreground"
               >
-                Products
+                Projects
               </Link>
               <Link
                 href="#"
                 className="text-muted-foreground hover:text-foreground"
               >
-                Customers
+                Resume
               </Link>
               <Link href="#" className="hover:text-foreground">
-                Settings
+                Contact Me
               </Link>
+              <ThemeToggle/>
             </nav>
           </SheetContent>
         </Sheet>
-        <ThemeToggle/>
       </header>
 );}
